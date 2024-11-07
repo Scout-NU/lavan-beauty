5e7de038729b08e8c3c7b2784c0c3f73ae426952
emilylin1 <lin.emi@northeastern.edu>
2024-11-07 13:41:01 -0500

    night cream lofi

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import './App.css';
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/home/Home';
+import NightCream from './pages/NightCream/NightCream';
 
 function App() {
   return (
     <div>
         <BrowserRouter>
         <Routes>
-            <Route path="/" element={<Home />} />
+            <Route path="/" element={<NightCream />} />
         </Routes>
         </BrowserRouter>
     </div>

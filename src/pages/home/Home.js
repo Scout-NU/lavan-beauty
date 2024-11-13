5961ab2122f45a6bfbe7aa232191d755758ddd69
emilylin1 <lin.emi@northeastern.edu>
2024-11-12 21:29:25 -0500

    home page hifi

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,4 +1,9 @@
 import './Home.css';
+import left_landing from '../../assets/home/left_landing.png';
+import right_landing from '../../assets/home/right_landing.png';
+import product_image from '../../assets/home/product_image.png';
+import pharmacist from '../../assets/home/pharmacist.png';
+import bottom_image from '../../assets/home/bottom_image.png';
 import placeholder from '../../assets/placeholder.jpg';
 
 function Home() {
@@ -6,60 +11,42 @@ function Home() {
     <div className="home">
       <div className="landing">
         <div className="landing-day-cream">
-          <img src={placeholder} alt="Day Cream" />
-          <h1>Day Cream Name</h1>
-          <h3>Lorem ipsum dolor sit amet, consectetur adipiscing</h3>
+          <img src={left_landing} alt="Day Cream" />
         </div>
         <div className="landing-night-cream">
-          <img src={placeholder} alt="Night Cream" />
-          <h1>Night Cream Name</h1>
-          <h3>Lorem ipsum dolor sit amet, consectetur adipiscing</h3>
+          <img src={right_landing} alt="Night Cream" />
         </div>
       </div>
       <div className="landing-text">
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco aboris nisi ut aliquip ex ea commodo consequat. </p>
+        Where luxurious skincare meets nature’s finest ingredients
       </div>
       <div className="product">
-        <img src={placeholder} alt="Day and Night Cream Set" />
+        <img src={product_image} alt="Day and Night Cream Set" />
         <div className="product-info">
           <h1>Day & Night Set Name</h1>
-          <div className="product-details">
-            <span className="price">
-              $XX
-            </span>
-            <span className="sizes">
-              <svg xmlns="http://www.w3.org/2000/svg" width="21" height="21" viewBox="0 0 21 21" fill="none">
-                <circle cx="10.5" cy="10.5" r="9.5" fill="#D9D9D9" stroke="black" stroke-width="2" />
-              </svg> XXml
-              <svg xmlns="http://www.w3.org/2000/svg" width="21" height="21" viewBox="0 0 21 21" fill="none">
-                <circle cx="10.5" cy="10.5" r="9.5" fill="#D9D9D9" stroke="#D9D9D9" stroke-width="2" />
-              </svg> XXml
-            </span>
+          <div className="product-price">
+            $36
           </div>
           <div className="product-description">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            Experience the ultimate skincare transformation with our Day & Night Cream Duo, designed to nourish and revitalize your skin around the clock.
           </div>
-          <div className="add-to-bag">
-            <a className="add-to-bag" href="/">Add to bag</a>
+          <div className="view-product">
+            <a className="view-product" href="/">View Product</a>
           </div>
         </div>
       </div>
       <div className="pharmacist">
         <div className="pharmacist-text">
           <h1>Made by a pharmacist</h1>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          Crafted by a skilled pharmacist, our Day & Night Cream Duo combines expert knowledge with high-quality ingredients to deliver effective, trustworthy skincare.
         </div>
-        <img src={placeholder} alt="Pharmacist" />
+        <img src={pharmacist} alt="Pharmacist" />
       </div>
       <div className="bottom-image">
-        <img src={placeholder} alt="" />
-      </div>
-      <div className="logo">
-        <h1>Lavan Beauty Logo</h1>
+        <img src={bottom_image} alt="" />
       </div>
     </div>
   );
 }
 
 export default Home;
- 
